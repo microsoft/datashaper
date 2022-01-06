f1452cac77ba41060d0a6fcc7d80c010ba3cfff7
Monica Carvajal <mcarvajal@microsoft.com>
2022-01-06 18:51:54 -0500

    jest.config updated to run jsdom and node tests

--- packages/utilities/utils/__tests__/files.test.ts
+++ packages/utilities/utils/__tests__/files.test.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+
 import { isDsvFile, isJsonFile, isZipFile } from '..'
 import { BaseFile, FileWithPath } from '../../common'
 import {
--- packages/utilities/utils/__tests__/zip.test.ts
+++ packages/utilities/utils/__tests__/zip.test.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+
 import * as zip from '@zip.js/zip.js'
 import { FileWithPath } from '../../common'
 import { getBlobFromEntry, getFileFromEntry, toZip } from '../zip'
