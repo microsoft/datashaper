f9132de854aca767788ae44147e268aa2b4cad4e
Nathan Evans <github@talkswithnumbers.com>
2022-01-06 11:43:44 -0800

    Simplify Rename component

--- packages/react/src/controls/dropdowns/TableColumnDropdown.tsx
+++ packages/react/src/controls/dropdowns/TableColumnDropdown.tsx
@@ -11,14 +11,19 @@ import { columnDropdownStyles } from '../styles'
 
 export interface TableColumnDropdownProps extends Partial<IDropdownProps> {
 	table?: ColumnTable
+	/**
+	 * Optional filter function to apply to the column list.
+	 * Passed directly to table.columnNames()
+	 */
+	filter?: (name: string) => boolean
 }
 
 /**
  * Dropdown wrapper to automatically list the columns of an Arquero table.
  */
 export const TableColumnDropdown: React.FC<TableColumnDropdownProps> = memo(
-	function TableColumnDropdown({ table, ...rest }) {
-		const options = useTableColumnOptions(table)
+	function TableColumnDropdown({ table, filter, ...rest }) {
+		const options = useTableColumnOptions(table, filter)
 		return (
 			<Dropdown
 				label={'Column'}
