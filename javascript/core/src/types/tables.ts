b21f071ebcf11c935df8fb568de495c99d5b5ee2
Nathan Evans <github@talkswithnumbers.com>
2022-02-22 18:09:18 -0800

    Add metadata and context fields to TableContainer

--- javascript/core/src/types/tables.ts
+++ javascript/core/src/types/tables.ts
@@ -58,7 +58,7 @@ export interface TableMetadata {
  */
 export type ResolverFunction = (id: string) => Promise<ColumnTable>
 
-export interface TableContainer {
+export interface TableContainer<T = unknown> {
 	/**
 	 * This is the formal id for a table, and must be unique within the store.
 	 * A URI would normally be appropriate.
@@ -73,4 +73,15 @@ export interface TableContainer {
 	 * If it has not been resolved yet it will be undefined.
 	 */
 	table?: ColumnTable
+	/**
+	 * Optional metadata block for the table.
+	 * This can follow the container around to help avoid recomputes.
+	 */
+	metadata?: TableMetadata
+	/**
+	 * Optional contextual data about the table.
+	 * For example, if the table was created using a process/transformation
+	 * that has additional information to relay, that context can be stored here.
+	 */
+	context?: T
 }
