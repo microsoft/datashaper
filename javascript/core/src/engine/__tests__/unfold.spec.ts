670448163707b866dbf19beffb45363c996c641c
gaudyb <85708998+gaudyb@users.noreply.github.com>
2022-03-01 04:22:26 +0000

    new tests implemented

--- javascript/core/src/engine/__tests__/unfold.spec.ts
+++ javascript/core/src/engine/__tests__/unfold.spec.ts
@@ -8,7 +8,43 @@ import { unfold } from '../verbs/unfold'
 import { TestStore } from './TestStore'
 
 describe('test for unfold verb', () => {
-	test('unfold test with multiple columns', async () => {
+	test('unfold test with one column folded', async () => {
+		const step: Step = {
+			verb: Verb.Fold,
+			input: 'table10',
+			output: 'newTable',
+			args: { to: ['key', 'value'], columns: ['x'] },
+		}
+
+		const store = new TestStore()
+
+		await fold(step, store).then(result => {
+			store.set(result)
+		})
+
+		const step2: Step = {
+			verb: Verb.Unfold,
+			input: 'newTable',
+			output: 'output',
+			args: { key: 'key', value: 'value' },
+		}
+
+		return unfold(step2, store).then(result => {
+			expect(result.table.numCols()).toBe(3)
+			expect(result.table.numRows()).toBe(3)
+			expect(result.table.get('x', 0)).toBe('A')
+			expect(result.table.get('x', 1)).toBe('B')
+			expect(result.table.get('x', 2)).toBe('A')
+			expect(result.table.get('y', 0)).toBe(1)
+			expect(result.table.get('y', 1)).toBe(2)
+			expect(result.table.get('y', 2)).toBe(1)
+			expect(result.table.get('z', 0)).toBe(4)
+			expect(result.table.get('z', 1)).toBe(5)
+			expect(result.table.get('z', 2)).toBe(4)
+		})
+	})
+
+	test('unfold test with all columns folded', async () => {
 		const step: Step = {
 			verb: Verb.Fold,
 			input: 'table18',
@@ -41,6 +77,78 @@ describe('test for unfold verb', () => {
 			expect(result.table.get('C', 0)).toBe(3)
 			expect(result.table.get('C', 1)).toBe(5)
 			expect(result.table.get('C', 2)).toBe(30)
+		})
+	})
+
+	test('unfold test with value on value column undefined', async () => {
+		const step: Step = {
+			verb: Verb.Fold,
+			input: 'table14',
+			output: 'newTable',
+			args: { to: ['key', 'value'], columns: ['y', 'z'] },
+		}
+
+		const store = new TestStore()
+
+		await fold(step, store).then(result => {
+			store.set(result)
+		})
+
+		const step2: Step = {
+			verb: Verb.Unfold,
+			input: 'newTable',
+			output: 'output',
+			args: { key: 'key', value: 'value' },
+		}
+
+		return unfold(step2, store).then(result => {
+			expect(result.table.numCols()).toBe(3)
+			expect(result.table.numRows()).toBe(3)
+			expect(result.table.get('x', 0)).toBe('A')
+			expect(result.table.get('x', 1)).toBe('B')
+			expect(result.table.get('x', 2)).toBe('A')
+			expect(result.table.get('y', 0)).toBe(1)
+			expect(result.table.get('y', 1)).toBeUndefined()
+			expect(result.table.get('y', 2)).toBe(1)
+			expect(result.table.get('z', 0)).toBe(true)
+			expect(result.table.get('z', 1)).toBe(false)
+			expect(result.table.get('z', 2)).toBe(false)
+		})
+	})
+
+	test('unfold test with value on value column null', async () => {
+		const step: Step = {
+			verb: Verb.Fold,
+			input: 'table15',
+			output: 'newTable',
+			args: { to: ['key', 'value'], columns: ['y', 'z'] },
+		}
+
+		const store = new TestStore()
+
+		await fold(step, store).then(result => {
+			store.set(result)
+		})
+
+		const step2: Step = {
+			verb: Verb.Unfold,
+			input: 'newTable',
+			output: 'output',
+			args: { key: 'key', value: 'value' },
+		}
+
+		return unfold(step2, store).then(result => {
+			expect(result.table.numCols()).toBe(3)
+			expect(result.table.numRows()).toBe(3)
+			expect(result.table.get('x', 0)).toBe('A')
+			expect(result.table.get('x', 1)).toBe('B')
+			expect(result.table.get('x', 2)).toBe('A')
+			expect(result.table.get('y', 0)).toBeNull()
+			expect(result.table.get('y', 1)).toBe(true)
+			expect(result.table.get('y', 2)).toBe(false)
+			expect(result.table.get('z', 0)).toBe(true)
+			expect(result.table.get('z', 1)).toBe(false)
+			expect(result.table.get('z', 2)).toBe(true)
 		})
 	})
 })
