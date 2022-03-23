056bd3a8e5a11ed2c3afd62d12f682a600776d35
Chris Trevino <chtrevin@microsoft.com>
2022-03-23 11:18:42 -0700

    remove some unnecessary string casts

--- javascript/core/src/engine/verbs/join.ts
+++ javascript/core/src/engine/verbs/join.ts
@@ -42,10 +42,7 @@ class JoinNode extends BaseNode<TableContainer, JoinArgs> {
 		const right = this.inputValue(JoinInput.Right)
 		if (left?.table != null && right?.table != null && this.config != null) {
 			this.emit(
-				container(
-					String(this.id),
-					doJoin(left.table, right.table, this.config),
-				),
+				container(this.id, doJoin(left.table, right.table, this.config)),
 			)
 		} else {
 			this.emit(undefined)
