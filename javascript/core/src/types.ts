7d6293ac9e88920f94a10adfe5a03380b6422bee
Chris Trevino <chtrevin@microsoft.com>
2022-03-24 13:32:39 -0700

    update core outputs

--- javascript/core/src/types.ts
+++ javascript/core/src/types.ts
@@ -2,4 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-export * from './types/index.js'
+import type { Store } from './store/types.js'
+import type { TableContainer } from './tables/types.js'
+
+export type TableStore = Store<TableContainer>
