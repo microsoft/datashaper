29bb4924c26b685d615c9d5ac01ad5bda6755855
Chris Trevino <chtrevin@microsoft.com>
2022-05-06 16:20:06 -0700

    DebugPage refactors

---
+++ javascript/webapp/src/pages/DebugPage/Section.styles.ts
@@ -0,0 +1,42 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { Separator } from '@fluentui/react'
+import styled from 'styled-components'
+
+export const Container = styled.section`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	margin-bottom: 40px;
+`
+
+export const Titles = styled.div`
+	width: 200px;
+	text-align: right;
+	text-transform: uppercase;
+`
+
+export const H1 = styled.h1`
+	margin-top: 0;
+	margin-bottom: 0;
+	color: ${({ theme }) => theme.application().lowContrast().hex()};
+`
+
+export const H2 = styled.h2`
+	margin-top: 0;
+	margin-bottom: 0;
+	color: ${({ theme }) => theme.application().accent().hex()};
+`
+
+export const StyledSeparator = styled(Separator)`
+	margin-left: 8px;
+	margin-right: 36px;
+`
+
+export const ChildrenContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`
