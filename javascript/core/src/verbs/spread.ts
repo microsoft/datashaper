0e93e111c74dd14c083a71b3eb33caaf54ff902c
Nathan Evans <github@talkswithnumbers.com>
2022-05-31 17:36:29 -0700

    Fix spread type inheritance

--- javascript/core/src/verbs/spread.ts
+++ javascript/core/src/verbs/spread.ts
@@ -2,11 +2,11 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import type { InputColumnArgs } from './types.js'
 import type { ColumnTableStep } from './util/factories.js'
 import { stepVerbFactory } from './util/factories.js'
 
-export interface SpreadArgs {
-	column: string
+export interface SpreadArgs extends InputColumnArgs {
 	to: string[]
 }
 
