27d363005e5c42dc4773044f993beb70c5882514
Nathan Evans <github@talkswithnumbers.com>
2022-06-02 16:15:45 -0700

    Add onehot param

--- javascript/core/src/verbs/spread.ts
+++ javascript/core/src/verbs/spread.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { escape, op } from 'arquero'
+import { escape, not, op } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table.js'
 
 import type { InputColumnArgs } from './types.js'
@@ -11,15 +11,26 @@ import { stepVerbFactory } from './util/factories.js'
 
 export interface SpreadArgs extends InputColumnArgs {
 	to: string[]
+	/**
+	 * Delimiter to use when converting string cell values into an array with String.split
+	 */
 	delimiter?: string
+	/**
+	 * Indicates that a onehot-style spread should be performed.
+	 * This maps all unique cell values to new columns and sets the output cell value to a binary 1/0 based on column match.
+	 * This is in contrast to the default spread, which just maps array values to column by index.
+	 */
+	onehot?: boolean
 }
 
 export const spreadStep: ColumnTableStep<SpreadArgs> = (
 	input,
-	{ to, column, delimiter },
+	{ to, column, delimiter, onehot },
 ) => {
 	const split = applySplit(input, [column], delimiter)
-	return applyDefaultSpread(split, [column], to)
+	return onehot
+		? applyOnehotSpread(split, [column])
+		: applyDefaultSpread(split, [column], to)
 }
 
 // if a delimiter is supplied, splits the cell values of every required column in the table
@@ -33,6 +44,7 @@ function applySplit(
 		const args = columns.reduce((acc, col) => {
 			acc[col] = escape((d: any) => op.split(d[col], delimiter, undefined))
 			return acc
+			// TODO: expression type
 		}, {} as Record<string, any>)
 		return table.derive(args)
 	}
@@ -42,8 +54,48 @@ function applySplit(
 // creates a default arquero spread operation for each column
 // this ignores unique values and just creates a new column for each index value
 // note also that arquero only inspects the first valid cell, it does not find the longest array
-function applyDefaultSpread(table: ColumnTable, columns: string[], to?: string[]) {
+function applyDefaultSpread(
+	table: ColumnTable,
+	columns: string[],
+	to?: string[],
+) {
 	return table.spread(columns, { as: to })
+}
+
+// applies the onehot spread, assuming column cells already contain arrays
+function applyOnehotSpread(table: ColumnTable, columns: string[]): ColumnTable {
+	// collect all of the unique values for each onehot column
+	const hash = createUniqueColumnValuesHash(table, columns)
+	const args = columns.reduce((acc, col) => {
+		const values = hash[col]!
+		Object.keys(values)
+			.sort()
+			.forEach((value: any) => {
+				// each unique value should result in a new column
+				acc[`${col}_${value}`] = escape((d: any) => {
+					return op.includes(d[col], value, undefined) ? 1 : 0
+				})
+			})
+		return acc
+	}, {} as Record<string, any>)
+
+	// note the final select: arquero's spread eliminates the original columns
+	return table.derive(args).select(not(columns))
+}
+
+// for each column, collect the unique values in a hash
+function createUniqueColumnValuesHash(
+	table: ColumnTable,
+	columns: string[],
+): Record<string, object> {
+	// TODO: there's probably a more efficient method than unrolling everything first
+	const unrolled = table.unroll(columns)
+	const args = columns.reduce((acc, col) => {
+		acc[col] = op.object_agg(col, col)
+		return acc
+	}, {} as Record<string, object>)
+	const collapsed = unrolled.rollup(args)
+	return collapsed.object(0) as Record<string, object>
 }
 
 export const spread = stepVerbFactory(spreadStep)
