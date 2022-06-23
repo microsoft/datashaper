ab8d69373c101e2f2aa9862869723de658d564ae
Monica Carvajal <mcarvajal@microsoft.com>
2022-06-23 10:58:55 -0500

    onehot python

--- python/data-wrangling-components/data_wrangling_components/engine/verbs/onehot.py
+++ python/data-wrangling-components/data_wrangling_components/engine/verbs/onehot.py
@@ -3,16 +3,31 @@
 # Licensed under the MIT license. See LICENSE file in the project.
 #
 
+from typing import List
 import pandas as pd
 
 from data_wrangling_components.engine.verbs.verb_input import VerbInput
 from data_wrangling_components.table_store import TableContainer
 
 
-def onehot(input: VerbInput, column: str, prefix: str = ""):
+def onehot(input: VerbInput, columns: List[str], prefixes: List[str] = []):
+    def hasPrefix(index: int):
+        try:
+            return prefixes[index] != '' and prefixes[index] != None
+        except IndexError:
+            return False
+
+    def getPrefix(index: int):
+        if hasPrefix(index):
+            return prefixes[index]
+        else:
+            return columns[index] + "_"
+
     input_table = input.get_input()
-    dummies = pd.get_dummies(input_table[column], prefix=prefix, prefix_sep="")
-    dummies.loc[input_table[column].isnull(), dummies.columns] = None
+    prefixes = [getPrefix(index) for index in range(len(columns))]
+
+    dummies = pd.get_dummies(input_table[columns], prefix=prefixes, prefix_sep="")
+    dummies.loc[input_table[columns].isnull(), dummies.columns] = None
     output = pd.concat([input_table, dummies], axis=1)
 
     return TableContainer(table=output)
