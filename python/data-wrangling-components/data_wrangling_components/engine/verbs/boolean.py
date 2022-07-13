a80536d5aee8f9c4fbd4526b22326e26168f471c
gaudyb <85708998+gaudyb@users.noreply.github.com>
2022-07-13 17:40:55 +0000

    python boolean operators logic changes

--- python/data-wrangling-components/data_wrangling_components/engine/verbs/boolean.py
+++ python/data-wrangling-components/data_wrangling_components/engine/verbs/boolean.py
@@ -16,6 +16,6 @@ def boolean(input: VerbInput, to: str, columns: List[str], operator: str):
     logical_operator = BooleanLogicalOperator(operator)
     input_table = input.get_input()
     output = input_table.copy()
-    output[to] = _boolean_function_map[logical_operator](output)
+    output[to] = _boolean_function_map[logical_operator](output, columns)
 
     return TableContainer(table=output)
