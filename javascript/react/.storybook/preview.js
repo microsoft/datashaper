2f1bc1d306cd37bc38ff91a5c74d4737ed25e2b7
Nathan Evans <github@talkswithnumbers.com>
2022-08-11 16:55:30 -0700

    Migrate some storybook config

--- javascript/react/.storybook/preview.js
+++ javascript/react/.storybook/preview.js
@@ -1,14 +1,23 @@
 import { addDecorator } from '@storybook/react'
 import { ThematicFluentDecorator } from './ThematicFluentDecorator'
+import { loadCSV } from 'arquero'
 
 addDecorator(ThematicFluentDecorator)
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 }
+
+export const loaders = [
+	async () => ({
+		stocks: await loadCSV('./data/stocks.csv', {
+			autoMax: 1000000,
+		}),
+	}),
+]
