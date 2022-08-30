79943eb1853392a68489bf4b6fcedccb2922b143
Nathan Evans <github@talkswithnumbers.com>
2022-08-30 13:49:28 -0700

    Flatten load methods

--- javascript/tables/src/load/validators.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-
-export function validateCharacterLength(char?: string, length = 1) {
-	return char ? char.length === length : true
-}
