f71ae3a7a1eab23d2381f7e1f57389e12ead032b
gaudyb <85708998+gaudyb@users.noreply.github.com>
2022-09-02 20:52:34 +0000

    yarn ci fixes

--- javascript/tables/src/generateCodebook.ts
+++ javascript/tables/src/generateCodebook.ts
@@ -2,27 +2,28 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { Codebook, Field } from '@datashaper/schema'
+import type { Codebook } from '@datashaper/schema'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
 
 import { guessDataTypeByColumn } from './guessDataTypeByColumn.js'
 
 export function generateCodebook(table: ColumnTable): Codebook {
-	const codebookResult: Codebook = {}
-	const fieldResults: Field[] = []
+	const codebookResult: Codebook = {
+		$schema: '???',
+		name: 'Generator',
+		fields: [],
+	}
 
 	table.columnNames().forEach(column => {
 		const columnType = guessDataTypeByColumn(table.array(column))
 
-		const field: Field = {
+		const field = {
 			name: column,
 			type: columnType,
 		}
 
-		fieldResults.push(field)
+		codebookResult.fields.push(field)
 	})
-
-	codebookResult.fields = fieldResults
 
 	return codebookResult
 }
