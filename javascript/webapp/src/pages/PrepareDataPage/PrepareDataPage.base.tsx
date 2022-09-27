0b5338b2b3c773b9e797e65e60f0501bfade9494
Dayenne de Souza <ddesouza@microsoft.com>
2022-09-27 18:02:03 -0300

    fix

--- javascript/webapp/src/pages/PrepareDataPage/PrepareDataPage.base.tsx
+++ javascript/webapp/src/pages/PrepareDataPage/PrepareDataPage.base.tsx
@@ -23,7 +23,8 @@ import type { TableContainer } from '@datashaper/tables'
 import { Workflow } from '@datashaper/workflow'
 import type { IColumn } from '@fluentui/react'
 import { useBoolean } from '@fluentui/react-hooks'
-import { memo, useCallback, useState } from 'react'
+import upperFirst from 'lodash-es/upperFirst.js'
+import { memo, useCallback, useMemo, useState } from 'react'
 
 import { useStepListener, useTables } from './PrepareDataPage.hooks.js'
 import {
@@ -57,6 +58,12 @@ export const PrepareDataPage: React.FC = memo(function PrepareDataPage() {
 	const onSave = useOnSaveStep(workflow)
 	const onCreate = useOnCreateStep(onSave, setSelectedTableId)
 	const inputNames = useInputTableNames(workflow)
+
+	const tableName = useMemo(() => {
+		const stepIndex = workflow.steps.findIndex(x => x.id === selectedTableId)
+		const name = upperFirst(workflow.steps[stepIndex]?.verb)
+		return stepIndex >= 0 ? `#${stepIndex + 1} ${name}` : selectedTableId
+	}, [workflow, selectedTableId])
 
 	useStepListener(workflow, setSelectedTableId, inputNames)
 	useWorkflowOutputListener(workflow, setOutputs)
@@ -117,6 +124,7 @@ export const PrepareDataPage: React.FC = memo(function PrepareDataPage() {
 												onRemoveStep={onDelete}
 											/>
 										}
+										name={tableName}
 										table={selectedTable?.table}
 									/>
 									<ArqueroDetailsList
