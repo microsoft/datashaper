db99b25ed24558ad50ad3a9b96212000c973358d
Chris Trevino <darthtrevino@gmail.com>
2022-10-03 09:24:41 -0700

    update the datasource model to use the readTable primitive in @datashaper/tables

--- javascript/tables/src/readTable.ts
+++ javascript/tables/src/readTable.ts
@@ -2,21 +2,44 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-
 import type { ParserOptions } from '@datashaper/schema'
+import { DataFormat } from '@datashaper/schema'
+import { fromJSON } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
-import { guessDelimiter } from './guessDelimiter.js'
-import { COMMENT_DEFAULT, DELIMITER_DEFAULT } from './typeHints.defaults.js'
-import { getParser, validOptions } from './readTable.utils.js'
 
-const defaultParserOptions = {
+import { guessDelimiter } from './guessDelimiter.js'
+import { getParser, validOptions } from './readTable.utils.js'
+import { COMMENT_DEFAULT, DELIMITER_DEFAULT } from './typeHints.defaults.js'
+
+const DEFAULT_PARSER_OPTIONS: ParserOptions = {
 	delimiter: DELIMITER_DEFAULT,
 	comment: COMMENT_DEFAULT,
 }
 
-export function readTable(
+/**
+ * Read an input table
+ * @param input - The input blob
+ * @param format - The data format of the table (e.g. JSON, CSV, Arrow)
+ * @param options - The parsing options when using CSV
+ * @returns
+ */
+export async function readTable(
+	input: Blob,
+	format: DataFormat,
+	options?: ParserOptions,
+): Promise<ColumnTable> {
+	if (format === DataFormat.JSON) {
+		return fromJSON(await input.text())
+	} else if (format === DataFormat.CSV) {
+		return readCsvTable(await input.text(), options)
+	} else {
+		throw new Error(`unknown data format: ${format}`)
+	}
+}
+
+export function readCsvTable(
 	text: string,
-	options: ParserOptions = defaultParserOptions,
+	options: ParserOptions = DEFAULT_PARSER_OPTIONS,
 ): ColumnTable {
 	const valid = validOptions(options)
 	if (!valid) {
