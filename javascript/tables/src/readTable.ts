1098f670d862c77d34eab7a3650b5e4a5260fd3a
Dayenne de Souza <ddesouza@microsoft.com>
2022-10-03 23:04:05 -0300

    add json shape to read table

--- javascript/tables/src/readTable.ts
+++ javascript/tables/src/readTable.ts
@@ -2,13 +2,21 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { ParserOptions } from '@datashaper/schema'
-import { DataFormat } from '@datashaper/schema'
-import { fromArrow, fromJSON } from 'arquero'
+import type {
+	DataShape,
+	ParserOptions} from '@datashaper/schema';
+import {
+	DataFormat,
+	DataOrientation
+} from '@datashaper/schema'
+import { from, fromArrow, fromJSON } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
 
 import { guessDelimiter } from './guessDelimiter.js'
-import { DEFAULT_PARSER_OPTIONS } from './readTable.constants.js'
+import {
+	DEFAULT_DATA_SHAPE_JSON_OPTIONS,
+	DEFAULT_PARSER_OPTIONS,
+} from './readTable.constants.js'
 import { getParser, validOptions } from './readTable.utils.js'
 
 /**
@@ -22,10 +30,11 @@ export async function readTable(
 	input: Blob,
 	format: DataFormat,
 	options?: ParserOptions,
+	shape?: DataShape,
 ): Promise<ColumnTable> {
 	switch (format) {
 		case DataFormat.JSON:
-			return fromJSON(await input.text())
+			return readJSONTable(await input.text(), shape)
 		case DataFormat.ARROW:
 			return fromArrow(await input.arrayBuffer())
 		case DataFormat.CSV:
@@ -48,4 +57,13 @@ export function readCsvTable(
 		delimiter: options?.delimiter || guessDelimiter(text),
 		...options,
 	})
+}
+export function readJSONTable(
+	text: string,
+	shapeOptions: DataShape = DEFAULT_DATA_SHAPE_JSON_OPTIONS,
+): ColumnTable {
+	if (shapeOptions.orientation === DataOrientation.Columnar) {
+		return fromJSON(text, {})
+	}
+	return from(JSON.parse(text) as JSON)
 }
