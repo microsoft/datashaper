f900fab3e5c1adf00917f060349d9c70a6b03bff
gaudyb <85708998+gaudyb@users.noreply.github.com>
2022-10-21 18:13:29 +0000

    fix metadata error

--- javascript/webapp/src/pages/PrepareDataPage/PrepareDataPage.base.tsx
+++ javascript/webapp/src/pages/PrepareDataPage/PrepareDataPage.base.tsx
@@ -107,6 +107,7 @@ export const PrepareDataPage: React.FC = memo(function PrepareDataPage() {
 									<TableCommands
 										inputTable={selectedTable}
 										workflow={workflow}
+										metadata={selectedTable.metadata}
 										onAddStep={onCreate}
 										selectedColumn={selectedColumn}
 										onRemoveStep={onDelete}
