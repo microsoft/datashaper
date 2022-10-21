05b6cb9e7c67eabac2028cd5264008e10f1bf499
Chris Trevino <darthtrevino@gmail.com>
2022-10-21 14:55:53 -0700

    add jest assertions

--- javascript/schema/src/__tests__/schemas.spec.ts
+++ javascript/schema/src/__tests__/schemas.spec.ts
@@ -3,9 +3,10 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-ignore */
-import { createSchemaValidator } from '../validator.js'
-import { createRequire } from 'module'
 import type Ajv from 'ajv'
+import { createRequire } from 'module'
+
+import { createSchemaValidator } from '../validator.js'
 const require = createRequire(import.meta.url)
 
 const CODEBOOK_SCHEMA_PATH = '../../../../schema/codebook.json'
@@ -14,21 +15,21 @@ const DATATABLE_SCHEMA_PATH = '../../../../schema/datatable.json'
 const WORKFLOW_SCHEMA_PATH = '../../../../schema/workflow.json'
 
 describe('Generated JSON Schemas', () => {
-	let validator: Ajv = createSchemaValidator()
+	const validator: Ajv = createSchemaValidator()
 
 	it('codebook schema can be validated', () => {
-		validator.compile(require(CODEBOOK_SCHEMA_PATH))
+		expect(validator.compile(require(CODEBOOK_SCHEMA_PATH))).toBeDefined()
 	})
 
 	it('datapackage schema can be validated', () => {
-		validator.compile(require(DATAPACKAGE_SCHEMA_PATH))
+		expect(validator.compile(require(DATAPACKAGE_SCHEMA_PATH))).toBeDefined()
 	})
 
 	it('datatable schema can be validated', () => {
-		validator.compile(require(DATATABLE_SCHEMA_PATH))
+		expect(validator.compile(require(DATATABLE_SCHEMA_PATH))).toBeDefined()
 	})
 
 	it('workflow schema can be validated', () => {
-		validator.compile(require(WORKFLOW_SCHEMA_PATH))
+		expect(validator.compile(require(WORKFLOW_SCHEMA_PATH))).toBeDefined()
 	})
 })
