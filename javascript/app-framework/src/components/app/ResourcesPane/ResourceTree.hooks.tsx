68b3db337b28fc83128393443bcf0e6ac8b81a93
Nathan Evans <github@talkswithnumbers.com>
2022-12-06 11:58:11 -0800

    Add starter Tree version

--- javascript/app-framework/src/components/app/ResourcesPane/ResourceTree.hooks.tsx
+++ javascript/app-framework/src/components/app/ResourcesPane/ResourceTree.hooks.tsx
@@ -2,14 +2,74 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import type { TreeItem } from '@essex/components'
 import type {
 	IContextualMenuItem,
 	INavLink,
 	INavLinkGroup,
 } from '@fluentui/react'
-import { useMemo } from 'react'
+import { useCallback,useMemo } from 'react'
 
 import type { ResourceRoute } from '../../../types.js'
+
+export function useTreeItems(resources: ResourceRoute[][]): TreeItem[] {
+	console.log('transforming resources', resources)
+	return useMemo<TreeItem[]>(() => {
+		const result: TreeItem[] = []
+		for (const group of resources) {
+			for (const resource of group) {
+				result.push(makeTreeItem(resource))
+			}
+		}
+		return result.filter(r => r.children.length > 0)
+	}, [resources])
+}
+
+export function useItemClick(
+	resources: ResourceRoute[][],
+	onSelect: (v: ResourceRoute) => void,
+) {
+	const map = useMemo(() => {
+		const result = new Map<string, ResourceRoute>()
+		const f = res => {
+			result.set(res.href, res)
+			for (const child of res.children ?? []) {
+				f(child)
+			}
+		}
+		for (const group of resources) {
+			for (const resource of group) {
+				f(resource)
+			}
+		}
+		return result
+	}, [resources])
+	const handleItemClick = useCallback(
+		(item: TreeItem) => {
+			console.log('click item', item)
+			const match = map.get(item.key)
+			onSelect(match)
+		},
+		[map, onSelect],
+	)
+	return handleItemClick
+}
+
+function makeTreeItem(
+	resource: ResourceRoute,
+): INavLink & { menuItems?: IContextualMenuItem[] } {
+	const numChildren = resource.children?.length ?? 0
+	return {
+		key: resource.href,
+		text: resource.title,
+		iconName: resource.icon,
+		children:
+			numChildren > 0
+				? resource.children?.map(c => makeTreeItem(c))
+				: undefined,
+		menuItems: resource.menuItems,
+	}
+}
 
 export function useNavGroups(
 	resources: ResourceRoute[][],
--- javascript/app-framework/src/components/app/ResourcesPane/ResourceTree.tsx
+++ javascript/app-framework/src/components/app/ResourcesPane/ResourceTree.tsx
@@ -2,28 +2,42 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import { Tree } from '@essex/components'
 import type {
 	IContextualMenuItem,
 	INavLink,
 	IRenderFunction,
 } from '@fluentui/react'
 import { Nav } from '@fluentui/react'
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
 
-import { useNavGroups } from './ResourceTree.hooks.js'
+import {
+	useItemClick,
+	useNavGroups,
+	useTreeItems,
+} from './ResourceTree.hooks.js'
 import type { ResourceTreeProps } from './ResourceTree.types.js'
 import { TreeLink } from './TreeLink.js'
 
 export const ResourceTree: React.FC<ResourceTreeProps> = memo(
 	function ResourceTree({ resources, selectedRoute, onSelect }) {
 		const navGroups = useNavGroups(resources, onSelect)
-
+		const treeItems = useTreeItems(resources)
+		console.log('tree items', treeItems)
+		const onItemClick = useItemClick(resources, onSelect)
 		return (
-			<Nav
+			<>
+				{/* <Nav
 				groups={navGroups}
 				selectedKey={selectedRoute}
 				onRenderLink={renderLink}
-			/>
+			/> */}
+				<Tree
+					items={treeItems}
+					selectedKey={selectedRoute}
+					onItemClick={onItemClick}
+				/>
+			</>
 		)
 	},
 )
