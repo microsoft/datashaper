46eb9e067e46b12317dcd6811392497c81b7172e
Nathan Evans <github@talkswithnumbers.com>
2023-01-03 22:18:37 +0000

    Force arquero fromCSV

--- javascript/tables/src/readTable.ts
+++ javascript/tables/src/readTable.ts
@@ -4,10 +4,10 @@
  */
 import type { DataTableSchema } from '@datashaper/schema'
 import { DataFormat } from '@datashaper/schema'
-import { fromArrow } from 'arquero'
+import { fromArrow, fromCSV } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
 
-import { readCsvTable } from './readCsvTable.js'
+import { mapToArqueroOptions } from './readCsvTable.utils.js'
 import { readJSONTable } from './readJSONTable.js'
 
 /**
@@ -24,13 +24,15 @@ export async function readTable(
 		return Promise.resolve(undefined)
 	}
 	const { format, shape, parser } = schema
+	const options = mapToArqueroOptions(parser)
 	switch (format) {
 		case DataFormat.JSON:
 			return readJSONTable(await input.text(), shape)
 		case DataFormat.ARROW:
 			return fromArrow(await input.arrayBuffer())
 		case DataFormat.CSV:
-			return readCsvTable(await input.text(), parser)
+			return fromCSV(await input.text(), options)
+		//return readCsvTable(await input.text(), parser)
 		default:
 			throw new Error(`unknown data format: ${format}`)
 	}
