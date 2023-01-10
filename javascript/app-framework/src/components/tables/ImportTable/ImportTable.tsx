5f4fd52743242f15db1229172f71be45e5baabdd
Nathan Evans <github@talkswithnumbers.com>
2023-01-10 21:19:26 +0000

    Extract ImportTable hooks

--- javascript/app-framework/src/components/tables/ImportTable/ImportTable.tsx
+++ javascript/app-framework/src/components/tables/ImportTable/ImportTable.tsx
@@ -2,15 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { DataFormat, DataOrientation } from '@datashaper/schema'
 import type { TableContainer } from '@datashaper/tables'
-import { generateCodebook,readTable } from '@datashaper/tables'
-import {
-	extension,
-	guessDelimiter,
-	removeExtension,
-} from '@datashaper/utilities'
-import { DataTable } from '@datashaper/workflow'
+import { generateCodebook } from '@datashaper/tables'
+import { removeExtension } from '@datashaper/utilities'
 import { ReadOnlyTextField } from '@essex/components'
 import {
 	Checkbox,
@@ -21,10 +15,15 @@ import {
 	TextField,
 } from '@fluentui/react'
 import type ColumnTable from 'arquero/dist/types/table/column-table.js'
-import { memo, useCallback, useEffect, useMemo, useState } from 'react'
+import { memo, useCallback, useState } from 'react'
 
 import { DataTableConfig } from '../../DataTableConfig/DataTableConfig.js'
 import { RawTable } from '../RawTable/RawTable.js'
+import {
+	useDraftSchema,
+	useFileAttributes,
+	usePreview,
+} from './ImportTable.hooks.js'
 import {
 	Footer,
 	Header,
@@ -44,74 +43,25 @@ const icons = {
 
 export const ImportTable: React.FC<ImportTableProps> = memo(
 	function ImportTable({ file, onCancel, onOpenTable }) {
-		// discover attributes of the content to craft a draft schema
-		const delimiter = useMemo(() => guessDelimiter(file.name), [file])
-		const fileExtension = useMemo(() => extension(file.path), [file])
-		const format = useMemo(
-			() => fileExtension.toLocaleLowerCase() as DataFormat,
-			[fileExtension],
-		)
+		const { delimiter, format, extension, content } = useFileAttributes(file)
 
 		const [name, setName] = useState<string>(removeExtension(file.name ?? ''))
-
-		const [rawContent, setRawContent] = useState<string | undefined>()
-		useEffect(() => {
-			file.toText().then(setRawContent)
-		}, [file])
-
 		const [autoType, setAutoType] = useState<boolean>(true)
-		const [table, setTable] = useState<ColumnTable | undefined>()
-		const [previewError, setPreviewError] = useState<string | undefined>()
 
-		const loadPreview = useCallback(
-			(schema: DataTable) => {
-				const f = async () => {
-					try {
-						const loadedTable = await readTable(file, schema.toSchema(), {
-							autoType,
-						})
-						setPreviewError(undefined)
-						setTable(loadedTable)
-					} catch (_) {
-						setPreviewError(
-							'The selected configuration is not valid for this table.',
-						)
-					}
-				}
-				void f()
-			},
-			[setTable, setPreviewError, file, autoType],
-		)
+		const { table, previewError, onLoadPreview } = usePreview(file, autoType)
 
-		const draftSchema = useMemo(() => {
-			const schema = new DataTable({
-				format,
-				parser: {
-					delimiter,
-				},
-				shape: {
-					orientation:
-						format === DataFormat.CSV
-							? DataOrientation.Values
-							: DataOrientation.Records,
-				},
-			})
-			schema.onChange(() => loadPreview(schema))
-			return schema
-		}, [format, delimiter, loadPreview])
-
-		useEffect(() => loadPreview(draftSchema), [draftSchema, loadPreview])
+		const draftSchema = useDraftSchema(delimiter, format, onLoadPreview)
 
 		const onClickImport = useCallback(() => {
 			if (table) {
-				const id = `${name}.${fileExtension}`
+				const id = `${name}.${extension}`
 				const tableContainer = { id, table } as TableContainer
 				// TODO: this is a bit inefficient, because a codebook is generated transitiviely in the readTable method when autoType is true
 				// we should separate those two functions. if a table is typed, generating the codebook can be much quicker.
 				const codebook = autoType ? generateCodebook(table) : undefined
 				onOpenTable(tableContainer, draftSchema.toSchema(), codebook)
 			}
-		}, [onOpenTable, table, name, fileExtension, draftSchema, autoType])
+		}, [onOpenTable, table, name, extension, draftSchema, autoType])
 
 		return (
 			<Modal styles={modalStyles} isOpen={true} onDismiss={onCancel} isBlocking>
@@ -133,7 +83,7 @@ export const ImportTable: React.FC<ImportTableProps> = memo(
 						<Label>Input text</Label>
 						<ReadOnlyTextField
 							multiline
-							value={rawContent}
+							value={content || ''}
 							styles={textFieldStyles}
 						/>
 						<Label>Final table</Label>
