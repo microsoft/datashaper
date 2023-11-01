b9daf546d2e0900e201aa03dafbcbed5d67b0571
Chris Trevino <chtrevin@microsoft.com>
2023-10-31 19:11:08 -0700

    remove debug log

--- python/datashaper/datashaper/workflow.py
+++ python/datashaper/datashaper/workflow.py
@@ -302,7 +302,6 @@ class Workflow(Generic[Context]):
 
             # set up verb progress reporter
             progress = create_verb_progress_reporter(f"verb: {verb_name}")
-            print("ProgressReporterFn=", progress)
             verb_reporter = VerbStatusReporter(
                 f"{self.name}.{verb_name}.{verb_idx}", status_reporter, progress
             )
