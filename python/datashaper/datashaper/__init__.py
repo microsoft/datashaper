d9e432b78f3230948af5a37decfba92dbaedc483
Chris Trevino <chtrevin@microsoft.com>
2024-01-08 17:33:40 -0800

    add default_input_name export

--- python/datashaper/datashaper/__init__.py
+++ python/datashaper/datashaper/__init__.py
@@ -3,7 +3,7 @@ from .engine.verbs import __all__ as verb_submodule
 from .execution import __all__ as execution_submodule
 from .progress import __all__ as progress_submodule
 from .table_store import __all__ as tablestore_submodule
-from .workflow import Workflow
+from .workflow import Workflow, DEFAULT_INPUT_NAME
 
 
 __all__ = [
@@ -12,4 +12,5 @@ __all__ = [
     *tablestore_submodule,
     *verb_submodule,
     "Workflow",
+    "DEFAULT_INPUT_NAME"
 ]
