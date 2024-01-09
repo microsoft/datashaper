6e01a0d24fee90ff52b692d5b109f705e0380de4
Chris Trevino <chtrevin@microsoft.com>
2024-01-08 17:35:12 -0800

    exports update

--- python/datashaper/datashaper/progress/__init__.py
+++ python/datashaper/datashaper/progress/__init__.py
@@ -1,6 +1,6 @@
 from .progress_callback import progress_callback
 from .progress_iterable import progress_iterable
 from .progress_ticker import progress_ticker
+from .reporters import __all__ as reporters_submodule
 
-
-__all__ = ["progress_callback", "progress_iterable", "progress_ticker"]
+__all__ = ["progress_callback", "progress_iterable", "progress_ticker", *reporters_submodule]
--- python/datashaper/datashaper/progress/reporters/__init__.py
+++ python/datashaper/datashaper/progress/reporters/__init__.py
@@ -1,8 +1,8 @@
-from .console_status_reporter import ConsoleStatusReporter  # noqa F401
-from .file_status_reporter import FileStatusReporter  # noqa F401
-from .noop_status_reporter import NoopStatusReporter  # noqa F401
-from .status_reporter import StatusReporter  # noqa F401
-from .verb_status_reporter import VerbStatusReporter  # noqa F401
+from .console_status_reporter import ConsoleStatusReporter
+from .file_status_reporter import FileStatusReporter
+from .noop_status_reporter import NoopStatusReporter
+from .status_reporter import StatusReporter
+from .verb_status_reporter import VerbStatusReporter
 
 
 __all__ = [
