47ed66b74e3e609383f766217a1edd387eaa5976
Chris Trevino <chtrevin@microsoft.com>
2024-01-08 17:37:25 -0800

    formatting

--- python/datashaper/datashaper/__init__.py
+++ python/datashaper/datashaper/__init__.py
@@ -3,7 +3,7 @@ from .engine.verbs import __all__ as verb_submodule
 from .execution import __all__ as execution_submodule
 from .progress import __all__ as progress_submodule
 from .table_store import __all__ as tablestore_submodule
-from .workflow import Workflow, DEFAULT_INPUT_NAME
+from .workflow import DEFAULT_INPUT_NAME, Workflow
 
 
 __all__ = [
@@ -12,5 +12,5 @@ __all__ = [
     *tablestore_submodule,
     *verb_submodule,
     "Workflow",
-    "DEFAULT_INPUT_NAME"
+    "DEFAULT_INPUT_NAME",
 ]
--- python/datashaper/datashaper/progress/__init__.py
+++ python/datashaper/datashaper/progress/__init__.py
@@ -3,4 +3,10 @@ from .progress_iterable import progress_iterable
 from .progress_ticker import progress_ticker
 from .reporters import __all__ as reporters_submodule
 
-__all__ = ["progress_callback", "progress_iterable", "progress_ticker", *reporters_submodule]
+
+__all__ = [
+    "progress_callback",
+    "progress_iterable",
+    "progress_ticker",
+    *reporters_submodule,
+]
