ade9ae17b5abcfb470fa07baf4e023db90f4a6a1
Chris Trevino <chtrevin@microsoft.com>
2024-01-08 19:10:28 -0800

    fix progress_ticker import

--- python/datashaper/datashaper/progress/progress_iterable.py
+++ python/datashaper/datashaper/progress/progress_iterable.py
@@ -1,8 +1,7 @@
 from typing import Iterable, TypeVar
 
-from datashaper.progress import progress_ticker
+from datashaper.progress.progress_ticker import progress_ticker
 from datashaper.progress.types import StatusReportHandler
-
 
 T = TypeVar("T")
 
