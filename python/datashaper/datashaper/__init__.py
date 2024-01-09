03c1f351e42c60d63369caec7358ee3abbd5f34a
Chris Trevino <chtrevin@microsoft.com>
2024-01-08 17:52:14 -0800

    formatting

--- python/datashaper/datashaper/__init__.py
+++ python/datashaper/datashaper/__init__.py
@@ -1,16 +1,49 @@
 from .engine import *  # noqa F401, F403
-from .engine.verbs import VerbInput, VerbDetails, verb, VerbManager, load_verbs
-from .execution import derive_from_rows, parallelize, VerbDefinitions, ExecutionNode
-from .progress import progress_callback, progress_iterable, progress_ticker, ConsoleStatusReporter, FileStatusReporter, NoopStatusReporter, StatusReporter, VerbStatusReporter
-from .table_store import ColumnStats, ColumnMetadata, TableMetadata, TableContainer, Table
+from .engine.verbs import VerbDetails, VerbInput, VerbManager, load_verbs, verb
+from .execution import ExecutionNode, VerbDefinitions, derive_from_rows, parallelize
+from .progress import (
+    ConsoleStatusReporter,
+    FileStatusReporter,
+    NoopStatusReporter,
+    StatusReporter,
+    VerbStatusReporter,
+    progress_callback,
+    progress_iterable,
+    progress_ticker,
+)
+from .table_store import (
+    ColumnMetadata,
+    ColumnStats,
+    Table,
+    TableContainer,
+    TableMetadata,
+)
 from .workflow import DEFAULT_INPUT_NAME, Workflow
 
 
 __all__ = [
-    "derive_from_rows", "parallelize", "VerbDefinitions", "ExecutionNode",
-    "VerbInput", "VerbDetails", "verb", "VerbManager", "load_verbs",
+    "derive_from_rows",
+    "parallelize",
+    "VerbDefinitions",
+    "ExecutionNode",
+    "VerbInput",
+    "VerbDetails",
+    "verb",
+    "VerbManager",
+    "load_verbs",
     "Workflow",
     "DEFAULT_INPUT_NAME",
-    "ColumnStats", "ColumnMetadata", "TableMetadata", "TableContainer", "Table",
-    "progress_callback", "progress_iterable", "progress_ticker", "ConsoleStatusReporter", "FileStatusReporter", "NoopStatusReporter", "StatusReporter", "VerbStatusReporter",
+    "ColumnStats",
+    "ColumnMetadata",
+    "TableMetadata",
+    "TableContainer",
+    "Table",
+    "progress_callback",
+    "progress_iterable",
+    "progress_ticker",
+    "ConsoleStatusReporter",
+    "FileStatusReporter",
+    "NoopStatusReporter",
+    "StatusReporter",
+    "VerbStatusReporter",
 ]
--- python/datashaper/datashaper/progress/__init__.py
+++ python/datashaper/datashaper/progress/__init__.py
@@ -1,7 +1,13 @@
 from .progress_callback import progress_callback
 from .progress_iterable import progress_iterable
 from .progress_ticker import progress_ticker
-from .reporters import (ConsoleStatusReporter, FileStatusReporter, NoopStatusReporter, StatusReporter, VerbStatusReporter)
+from .reporters import (
+    ConsoleStatusReporter,
+    FileStatusReporter,
+    NoopStatusReporter,
+    StatusReporter,
+    VerbStatusReporter,
+)
 
 
 __all__ = [
