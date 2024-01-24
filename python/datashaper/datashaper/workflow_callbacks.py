635eba2b9cd095fdb19deb84fe9f6b16b7f50f44
Andres Morales <andresmor@microsoft.com>
2024-01-24 16:41:39 -0600

    Update comments

--- python/datashaper/datashaper/workflow_callbacks.py
+++ python/datashaper/datashaper/workflow_callbacks.py
@@ -1,3 +1,4 @@
+"""Collection of callbacks that can be used to monitor the workflow execution."""
 from typing import Any, Dict, Optional, Protocol
 
 from datashaper.execution.execution_node import ExecutionNode
@@ -8,39 +9,39 @@ class WorkflowCallbacks(Protocol):
     """A collection of callbacks that can be used to monitor the workflow execution."""
 
     def on_workflow_start(self) -> None:
-        """Called when the workflow starts."""
+        """Executes when the workflow starts."""
         ...
 
     def on_step_start(self, node: ExecutionNode, inputs: Dict[str, Any]) -> None:
-        """Called when a step starts."""
+        """Executes every time a step starts."""
         ...
 
     def on_step_end(
         self, node: ExecutionNode, result: Optional[TableContainer]
     ) -> None:
-        """Called when a step ends."""
+        """Executes every time a step ends."""
         ...
 
     def on_workflow_end(self) -> None:
-        """Called when the workflow ends."""
+        """Executes when the workflow ends."""
         ...
 
 
 class NoOpCallbacks:
     def on_workflow_start(self) -> None:
-        """Called when the workflow starts."""
+        """Executes when the workflow starts."""
         pass
 
     def on_step_start(self, node: ExecutionNode, inputs: Dict[str, Any]) -> None:
-        """Called when a step starts."""
+        """Executes every time a step starts."""
         pass
 
     def on_step_end(
         self, node: ExecutionNode, result: Optional[TableContainer]
     ) -> None:
-        """Called when a step ends."""
+        """Executes every time a step ends."""
         pass
 
     def on_workflow_end(self) -> None:
-        """Called when the workflow ends."""
+        """Executes when the workflow ends."""
         pass
