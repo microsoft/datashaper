06bf8035516d5c9222e75879e200c1fd8c23768d
gaudyb <85708998+gaudyb@users.noreply.github.com>
2024-02-01 08:45:04 +0000

    python debugging process

--- python/datashaper/datashaper/engine/verbs/destructure.py
+++ python/datashaper/datashaper/engine/verbs/destructure.py
@@ -7,27 +7,11 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+import json
 
 from datashaper.engine.verbs.verb_input import VerbInput
 from datashaper.engine.verbs.verbs_mapping import verb
 from datashaper.table_store import TableContainer
-
-
-def _destructureSingleValue(
-    row, object: object, isArray: bool, prefix: str, keys: List[str] = []
-):
-    index = 0
-
-    for property, value in object.__dict__.items():
-        if (keys == None or len(keys) == 0) or (keys != None and property in keys):
-            if isArray:
-                row[prefix + index] = object[property]
-            else:
-                row[property] = object[property]
-
-            ++index
-
-    return row
 
 
 @verb(name="destructure")
@@ -43,16 +27,34 @@ def destructure(
 
     for index in output.index:
         if output[column][index] != None:
-            print("TEST GAUDY PYTHON")
-            print(isinstance(output[column][index], str))
-            output.loc[index] = _destructureSingleValue(
-                output.loc[index],
-                output[column][index],
-                type(output[column][index]) is list,
-                prefix,
-                keys,
-            )
+            counter = 0
+            object = None
+            isArray = False
 
+            print(output[column][index])
+
+            try:
+                if(json.loads(output[column][index])): #object
+                    object = json.loads(output[column][index])
+            except: #array
+                object = json.loads(json.dumps(output[column][index].split(",")))
+                isArray = True
+
+            if isArray:
+                print("Array test")
+                for i in range(len(object)):
+                    print(object[i])
+                    output.loc[index, prefix + str(counter)] = object[i]
+                    counter = counter + 1
+            else:
+                print("Object test")
+                for property in object:
+                    print(property)
+                    if (keys == None or len(keys) == 0) or (keys != None and property in keys):
+                        output.loc[index, property] = object[property] 
+                    
+    print(output)
+    
     filteredList: list[str] = []
 
     for col in output.columns:
@@ -61,5 +63,8 @@ def destructure(
 
     if not preserveSource:
         output = output[filteredList]
+
+    print("Final result")
+    print(output)
 
     return TableContainer(table=output)
