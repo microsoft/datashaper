f20f246eb2c915812632624514c977405b1f08cf
Andres Morales <andresmor@microsoft.com>
2024-02-12 13:46:06 -0600

    Update InMemory to use dataclass

--- python/datashaper/datashaper/table_store/in_memory_table_store.py
+++ python/datashaper/datashaper/table_store/in_memory_table_store.py
@@ -4,10 +4,18 @@
 #
 """The tablestore module contains the table store classes used by the datashaper."""
 
-from typing import cast
+from dataclasses import dataclass
 
 from datashaper.table_store.table_store import TableStore
 from datashaper.table_store.types import TableContainer
+
+
+@dataclass
+class Item:
+    """A simple item class."""
+
+    table_container: TableContainer
+    tag: str
 
 
 class InMemoryTableStore(TableStore):
@@ -15,15 +23,15 @@ class InMemoryTableStore(TableStore):
 
     def __init__(self) -> None:
         """Initialize the default table store."""
-        self._tables: dict[str, dict[str, TableContainer | str]] = {}
+        self._tables: dict[str, Item] = {}
 
     def add(self, name: str, table: TableContainer, tag: str = "default") -> None:
         """Add a table to the store."""
-        self._tables[name] = {"table": table, "tag": tag}
+        self._tables[name] = Item(table, tag)
 
     def get(self, name: str) -> TableContainer:
         """Get a table from the store."""
-        return cast(TableContainer, self._tables[name]["table"])
+        return self._tables[name].table_container
 
     def remove(self, name: str) -> None:
         """Remove a table from the store."""
@@ -33,9 +41,7 @@ class InMemoryTableStore(TableStore):
         """List the tables in the store."""
         if tag is None:
             return list(self._tables.keys())
-        return list(
-            [name for name, table in self._tables.items() if table["tag"] == tag]
-        )
+        return list([name for name, item in self._tables.items() if item.tag == tag])
 
     def dispose(self) -> None:
         """Clear the store."""
