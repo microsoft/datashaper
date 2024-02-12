2569951148c1ff7842abf44a40325c903b46752a
Andres Morales <andresmor@microsoft.com>
2024-02-12 11:30:08 -0600

    format

--- python/datashaper/datashaper/workflow/workflow.py
+++ python/datashaper/datashaper/workflow/workflow.py
@@ -55,13 +55,6 @@ class PandasDtypeBackend(str, Enum):
     PYARROW = "pyarrow"
 
 
-class PandasDtypeBackend(str, Enum):
-    """Pandas dtype backend."""
-
-    NUMPY_NULLABLE = "numpy_nullable"
-    PYARROW = "pyarrow"
-
-
 class Workflow(Generic[Context]):
     """A data processing graph."""
 
