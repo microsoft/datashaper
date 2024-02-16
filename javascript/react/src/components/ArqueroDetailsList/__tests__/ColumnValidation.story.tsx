21db6be56e6dffeeb053f004ed417622db54c4cc
Nathan Evans <github@talkswithnumbers.com>
2024-02-15 16:52:28 -0800

    Fix column validation story

--- javascript/react/src/components/ArqueroDetailsList/__tests__/ColumnValidation.story.tsx
+++ javascript/react/src/components/ArqueroDetailsList/__tests__/ColumnValidation.story.tsx
@@ -2,42 +2,50 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { ValidationResult } from '@datashaper/schema'
+import type { CodebookSchema, Constraints, ValidationResult } from '@datashaper/schema'
 import { generateCodebook, introspect, validateTable } from '@datashaper/tables'
 import type { ComponentStory } from '@storybook/react'
 
 import { ArqueroDetailsList } from '../ArqueroDetailsList.js'
 import type { ArqueroDetailsListProps } from '../ArqueroDetailsList.types.js'
+import { useEffect, useState } from 'react'
 
 export const ColumnValidationStory: ComponentStory<typeof ArqueroDetailsList> =
 	(
 		args: ArqueroDetailsListProps,
 		{ loaded: { companiesRaw } }: any,
 	): JSX.Element => {
-		const codebookResult = generateCodebook(companiesRaw)
+		const [codebook, setCodebook] = useState<CodebookSchema>()
+		useEffect(() => {
+			const f = async () => setCodebook(await generateCodebook(companiesRaw))
+			f()
+		}, [companiesRaw])
+		
 		const metadata = introspect(companiesRaw, true)
+
 		// inject a few constraints to test & render
-		let found = codebookResult.fields.find((e) => e.name === 'ID')
-		found.constraints = {
+		enrichField(codebook, 'ID', {
 			required: true,
 			unique: true,
 			minimum: 2,
 			maximum: 3,
-		}
-
-		found = codebookResult.fields.find((e) => e.name === 'Name')
-		found.constraints = {
+		})
+		enrichField(codebook, 'Name',{
 			enum: ['Microsoft', 'Apple', 'Google'],
-		}
+		})
+		enrichField(codebook, 'Employees', { minimum: 160000 })
 
-		found = codebookResult.fields.find((e) => e.name === 'Employees')
-		found.constraints = { minimum: 160000 }
-
-		const validationResult: ValidationResult = validateTable(
-			companiesRaw,
-			codebookResult,
-			false,
-		)
+		const [validationResult, setValidationResult] = useState<ValidationResult>()
+		useEffect(() => {
+			if (codebook) {
+				setValidationResult(validateTable(
+					companiesRaw,
+					codebook,
+					false,
+				))
+			}
+		}, [companiesRaw, codebook])
+		
 
 		return (
 			<ArqueroDetailsList
@@ -52,3 +60,10 @@ export const ColumnValidationStory: ComponentStory<typeof ArqueroDetailsList> =
 		)
 	}
 ColumnValidationStory.storyName = 'Column validation'
+
+function enrichField(codebook: CodebookSchema | undefined, name: string, constraints: Constraints) {
+	const found = codebook?.fields?.find((e) => e.name === name)
+	if (found) {
+		found.constraints = constraints
+	}
+}
