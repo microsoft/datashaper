79b39c0fb8eed15cf0f59b22b3f4c994d61ff785
Chris Trevino <darthtrevino@users.noreply.github.com>
2024-02-21 00:47:30 -0800

    Combine AsyncIO enumerations into a common one (#659)

--- python/datashaper/datashaper/execution/derive_from_rows.py
+++ python/datashaper/datashaper/execution/derive_from_rows.py
@@ -1,11 +1,11 @@
 """Apply a generic transform function to each row in a table."""
 import logging
 from collections.abc import Awaitable, Callable
-from enum import Enum
 from typing import TypeVar
 
 import pandas as pd
 
+from datashaper.engine.types import AsyncType
 from datashaper.workflow.verb_callbacks.verb_callbacks import VerbCallbacks
 
 from .derive_from_rows_asyncio import derive_from_rows_asyncio
@@ -15,27 +15,20 @@ logger = logging.getLogger(__name__)
 ItemType = TypeVar("ItemType")
 
 
-class AsyncIOType(str, Enum):
-    """Enum for the type of asyncio to use."""
-
-    AsyncIO = "asyncio"
-    AsyncIOThreads = "asyncio_threads"
-
-
 async def derive_from_rows(
     input: pd.DataFrame,
     transform: Callable[[pd.Series], Awaitable[ItemType]],
     callbacks: VerbCallbacks,
     num_threads: int = 4,
-    scheduling_type: AsyncIOType = AsyncIOType.AsyncIO,
+    scheduling_type: AsyncType = AsyncType.AsyncIO,
 ) -> list[ItemType | None]:
     """Apply a generic transform function to each row. Any errors will be reported and thrown."""
     match scheduling_type:
-        case AsyncIOType.AsyncIO:
+        case AsyncType.AsyncIO:
             return await derive_from_rows_asyncio(
                 input, transform, callbacks, num_threads
             )
-        case AsyncIOType.AsyncIOThreads:
+        case AsyncType.Threaded:
             return await derive_from_rows_asyncio_threads(
                 input, transform, callbacks, num_threads
             )
