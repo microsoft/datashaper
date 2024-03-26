af9adbeb7fe2816199ee9b0cb446510a25753223
Nathan Evans <github@talkswithnumbers.com>
2024-03-26 12:19:05 -0700

    Remove escape from erase verb

--- javascript/workflow/src/verbs/erase.ts
+++ javascript/workflow/src/verbs/erase.ts
@@ -3,7 +3,6 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import type { EraseArgs } from '@datashaper/schema'
-import { escape } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
 
 import type { ColumnTableStep } from './util/factories.js'
@@ -13,11 +12,15 @@ export const eraseStep: ColumnTableStep<EraseArgs> = (
 	input: ColumnTable,
 	{ value, column }: EraseArgs,
 ) => {
-	return input.derive({
-		[column]: escape((d: any) =>
-			`${d[column]}` === `${value}` ? null : d[column],
-		),
-	})
+	return input
+		.params({
+			column,
+			value,
+		})
+		.derive({
+			[column]: (d: any, $: any) =>
+				`${d[$.column]}` === `${$.value}` ? null : d[$.column],
+		})
 }
 
 export const erase = stepVerbFactory(eraseStep)
