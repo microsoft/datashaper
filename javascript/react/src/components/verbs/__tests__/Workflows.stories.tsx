b9306d30c1a8b3a5d8a136c2cf9de0b4047fb18f
Nathan Evans <github@talkswithnumbers.com>
2024-05-08 18:07:22 -0700

    Fix stories

--- javascript/react/src/components/verbs/__tests__/Workflows.stories.tsx
+++ javascript/react/src/components/verbs/__tests__/Workflows.stories.tsx
@@ -13,7 +13,6 @@ import multistepBinarize from './specs/multistep-binarize.json'
 import onehotunhot from './specs/onehot-unhot.json'
 import sparkbar from './specs/sparkbar.json'
 import sparkline from './specs/sparkline.json'
-import spreadhot from './specs/spreadhot.json'
 import { WorkflowExampleStory } from './WorkflowExample/WorkflowExample.story.js'
 
 const storyMetadata = {
@@ -97,10 +96,3 @@ export const Sparkline = {
 	},
 }
 
-export const Spreadhot = {
-	render: WorkflowExampleStory.bind({}),
-
-	args: {
-		schema: spreadhot as WorkflowSchema,
-	},
-}
