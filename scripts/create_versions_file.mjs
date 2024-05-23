6c9b4b00c88b4247ea877794928be3c6dfe54bd5
Chris Trevino <chtrevin@microsoft.com>
2024-05-23 11:48:33 -0700

    run formatter

--- scripts/create_versions_file.mjs
+++ scripts/create_versions_file.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import { writeFileSync } from 'fs'
-import path, { dirname } from 'path'
 import { createRequire } from 'module'
+import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 const require = createRequire(import.meta.url)
